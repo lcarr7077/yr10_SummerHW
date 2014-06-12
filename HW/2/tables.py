9fed4d60fbf3c082f9c7783d60f9f2c6b4e0f8e1
lcarr7077 <lcarr7077@sohamcollege.org.uk>
2014-06-12 18:44:52 +0100

    Update tables.py

--- HW/2/tables.py
+++ HW/2/tables.py
@@ -1,1 +1,7 @@
-#paste code here
+#User input
+n = int(input("What timestable would you like "))
+#x starts from 0
+x = 0
+#for loop to work out and show timestables
+for x in range(13):
+    print(x,"x",n,"=",n*x)
