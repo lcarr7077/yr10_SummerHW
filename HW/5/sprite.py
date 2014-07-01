7711f13c1bcb4f5b21dab3886cdaac9e53be7411
lcarr7077 <lcarr7077@sohamcollege.org.uk>
2014-07-01 21:53:29 +0100

    Update sprite.py
    I don't hnow the result as I couldn't get pygame working

--- HW/5/sprite.py
+++ HW/5/sprite.py
@@ -1,1 +1,34 @@
-#paste code here
+#imports pygame
+import pygame
+                
+
+pygame.init()
+
+#displays pygame screen
+screen = pygame.display.set_mode([225, 150])
+
+r = pygame.Color("red")
+w = pygame.Colour("white")
+
+#creates the whole image
+data = [
+    [ w, w, r, r, r, r, r, w, w ],
+    [ w, w, r, w, r, w, r, w, w ],
+    [ w, w, r, r, r, r, r, w, w ],
+    [ w, r, r, r, w, r, r, r, w ],
+    [ r, w, w, r, r, r, w, w, r ],
+    [ r, r, w, w, w, w, w, r, r ],
+    ]
+
+#rectangle
+for y, row in enumerate(data):
+    for x, colour in enumerate(row):
+        rect = pygame.Rect(x*25, y*25, 25, 25)
+        screen.fill(colour, rect=rect)
+
+pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit
